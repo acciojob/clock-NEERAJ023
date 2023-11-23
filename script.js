b7f58555168ddce6f68cf68641cbb22182bc82db
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-23 15:27:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let p=document.querySelector("#timer");
+function onload(){
+	let date=new Date();
+	p.innerText=`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()},${date.toLocaleTimeString()}`;
+}
+onload();
